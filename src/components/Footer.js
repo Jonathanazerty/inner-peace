1c8422512a98622d5734ee833ce628280c46a772
tanjamilisavljevic <70627722+tanjamilisavljevic@users.noreply.github.com>
2021-10-12 14:03:45 +0200

    Add waves animation to footer

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,11 +1,15 @@
 import React from "react";
+import './Footer.css';
 
 const Footer = () => {
     
     return (
-        
-        <body className="body">
-      </body>
+
+        <div className="ocean">
+            <div className="wave"/>
+            <div className="wave"/>
+        </div>
+
     )
 }
 
