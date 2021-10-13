34d9d44b9e2b0a3046cc7480a07b7d2e87179629
tanjamilisavljevic <70627722+tanjamilisavljevic@users.noreply.github.com>
2021-10-13 12:18:24 +0200

    Add Feeling slider, make vertical slider

--- src/components/Feeling.js
+++ src/components/Feeling.js
@@ -1,14 +1,32 @@
 import React from "react";
+import SentimentVeryDissatisfiedIcon from '@mui/icons-material/SentimentVeryDissatisfied';
+import SentimentDissatisfiedIcon from '@mui/icons-material/SentimentDissatisfied';
+import SentimentNeutralIcon from '@mui/icons-material/SentimentNeutral';
+import './Feeling.css';
+import SentimentSatisfiedIcon from '@mui/icons-material/SentimentSatisfied';
+import SentimentSatisfiedAltIcon from '@mui/icons-material/SentimentSatisfiedAlt';
+import VerticalSlider from "./FeelingSlider";
 
 
 const Feeling = () => {
-    
-    return (
-        
-        <body className="body">
 
-      </body>
+    return (
+        <>
+<VerticalSlider/>
+
+        </>
     )
 }
 
 export default Feeling;
+
+{/*<SentimentVeryDissatisfiedIcon/>*/
+}
+{/*<SentimentDissatisfiedIcon/>*/
+}
+{/*<SentimentNeutralIcon/>*/
+}
+{/*<SentimentSatisfiedIcon/>*/
+}
+{/*<SentimentSatisfiedAltIcon/>*/
+}
