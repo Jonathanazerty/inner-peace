85c8b9660ea03c862844e14d08d1447f737f951e
tanjamilisavljevic <70627722+tanjamilisavljevic@users.noreply.github.com>
2021-10-15 16:51:27 +0200

    Add styling for carousel

--- src/components/Entries.js
+++ src/components/Entries.js
@@ -11,11 +11,35 @@ const Entries = () => {
         slidesToScroll: 1
     };
     return (
-        <>
+        <div className='entriesMain'>
             <h2 className='previousEntries'> Previous Entries </h2>
             <Slider {...settings}>
                 <div>
-                    <h3>1</h3>
+                    <h3 className='entriesTitle'>Date.Date.2021.</h3>
+                    <div className='experiencingAndNotesWrapper' id='entriesCarouselWrapper'>
+                        <div className='experiencing'>
+                            <p>Experiencing:</p>
+                            <div className='symptoms'>
+                                <div>
+                                    <input type="checkbox" id="symptom1" name="symptom1" value="shortnessOfBreath"/>
+                                    <label htmlFor="symptom1"> Shortness of breath </label>
+                                </div>
+                                <div>
+                                    <input type="checkbox" id="symptom2" name="symptom2" value="heartPalpitations"/>
+                                    <label htmlFor="symptom2">Heart palpitations</label>
+                                </div>
+                                <div>
+                                    <input type="checkbox" id="symptom3" name="symptom3" value="nausea"/>
+                                    <label htmlFor="symptom3">Nausea</label>
+                                </div>
+                            </div>
+                        </div>
+
+                        <div className='notes'>
+                            <p><label htmlFor="notes">Notes:</label></p>
+                            <textarea className='symptoms' id="notes" name="notes"/>
+                        </div>
+                    </div>
                 </div>
                 <div>
                     <h3>2</h3>
@@ -33,7 +57,7 @@ const Entries = () => {
                     <h3>6</h3>
                 </div>
             </Slider>
-        </>
+        </div>
     )
 }
 
