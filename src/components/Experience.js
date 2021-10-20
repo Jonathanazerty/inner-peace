5b7189a7a229615e65d0199ac158bfa60a8e26b1
tanjamilisavljevic <70627722+tanjamilisavljevic@users.noreply.github.com>
2021-10-20 16:19:38 +0200

    Add symptoms

--- src/components/Experience.js
+++ src/components/Experience.js
@@ -1,11 +1,35 @@
-import React from 'react';
+import React, {useState} from 'react';
 import './Experience.css';
+import SentimentNeutralIcon from "@mui/icons-material/SentimentNeutral";
 
 function Experience() {
 
     const options = {weekday: 'long'}
     const date = new Date();
     let today = date.toLocaleDateString(undefined, options) + ' ' + date.getDate() + '-' + (date.getMonth() + 1) + '-' + date.getFullYear();
+
+    // const allChecked = [];
+    //
+    // const [feeling, setFeeling] = useState('')
+    // const [experiencing, setExperiencing] = useState('')
+    // const [notes, setNotes] = useState('')
+    //
+    //
+    // let newEntry = {
+    //     'Date' : today,
+    //     'Feeling' : '',
+    //     'Experiencing' : '',
+    //     'Notes' : ''
+    // }
+    //
+    // const makeChecked = () => {
+    //         allChecked.push()
+    // }
+    // const handleClick = () => {
+    //     setFeeling()
+    //     setExperiencing()
+    //     setNotes()
+    // }
 
     return (
         <div>
@@ -25,15 +49,53 @@ function Experience() {
                         <div className='symptoms'>
                             <div>
                                 <input type="checkbox" id="symptom1" name="symptom1" value="shortnessOfBreath"/>
-                                <label htmlFor="symptom1"> Shortness of breath </label>
+                                <> </>
+                                <label htmlFor="symptom1">Shortness of breath </label>
                             </div>
                             <div>
                                 <input type="checkbox" id="symptom2" name="symptom2" value="heartPalpitations"/>
+                                <> </>
                                 <label htmlFor="symptom2">Heart palpitations</label>
                             </div>
                             <div>
                                 <input type="checkbox" id="symptom3" name="symptom3" value="nausea"/>
+                                <> </>
                                 <label htmlFor="symptom3">Nausea</label>
+                            </div>
+                            <div>
+                                <input type="checkbox" id="symptom4" name="symptom4" value="trembling"/>
+                                <> </>
+                                <label htmlFor="symptom4">Trembling</label>
+                            </div>
+                            <div>
+                                <input type="checkbox" id="symptom5" name="symptom5" value="giproblems"/>
+                                <> </>
+                                <label htmlFor="symptom5">Gastrointestinal problems</label>
+                            </div>
+                            <div>
+                                <input type="checkbox" id="symptom6" name="symptom6" value="restlessness"/>
+                                <> </>
+                                <label htmlFor="restlessness">Restlessness</label>
+                            </div>
+                            <div>
+                                <input type="checkbox" id="symptom7" name="symptom7" value="worry"/>
+                                <> </>
+                                <label htmlFor="worry">Worry and apprehension</label>
+                            </div>
+                            <div>
+                                <input type="checkbox" id="symptom8" name="symptom8" value="tightness"/>
+                                <> </>
+                                <label htmlFor="tightness">Tightness in the throat and difficulty breathing</label>
+                            </div>
+                            <div>
+                                <input type="checkbox" id="symptom9" name="symptom9" value="faint"/>
+                                <> </>
+                                <label htmlFor="faint">Feeling faint</label>
+                            </div>
+                            <div>
+                                <input type="checkbox" id="symptom10" name="symptom10" value="headache"/>
+                                <> </>
+                                <label htmlFor="headache">Headache</label>
                             </div>
                         </div>
                     </div>
