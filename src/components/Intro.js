483ef830f13cb1dbfdb83826df329f633b901115
tanjamilisavljevic <70627722+tanjamilisavljevic@users.noreply.github.com>
2021-11-11 13:45:48 +0100

    Make intro responsive

--- src/components/Intro.js
+++ src/components/Intro.js
@@ -1,51 +1,48 @@
 import React, {useState, useEffect} from 'react';
 import './Intro.css';
 
-const Intro = ({ text }) => {
+const Intro = ({text}) => {
 
-  const [fadeProp, setFadeProp] = useState ({
+    const [fadeProp, setFadeProp] = useState({
         fade: 'fade-in',
-  });
+    });
 
-  useEffect (() => {
-      const timeout = setInterval(() => {
-          if(fadeProp.fade === 'fade-in'){
-            setFadeProp({
-              fade: 'fade-out'
-            })
-          } else {
-              window.location="/Name";
-          }
-      }, 2000);
+    useEffect(() => {
+        const timeout = setInterval(() => {
+            if (fadeProp.fade === 'fade-in') {
+                setFadeProp({
+                    fade: 'fade-out'
+                })
+            } else {
+                window.location = "/Name";
+            }
+        }, 2000);
 
-      return () => clearInterval(timeout)
+        return () => clearInterval(timeout)
     }, [fadeProp])
 
 
-  return (
+    return (
 
-      <div className={fadeProp.fade}  style={{
-        justifyContent: "center",
-        alignItems: "center",
-        textAlign: "center",
-        display: "flex",
-        flexFlow: "column",
-        minHeight: "80vh"
-        
-      }}>{text}
-        <div className="step" style={{
-            fontSize: "50px"
-      }}>
-          “ You’ve already taken the most important step,<br></br>
-          coming here “
-        </div><br></br>
-        <div className="name" style={{
-            fontSize: "20px"
-      }}>
-          - Inner Peace -
+        <div className={fadeProp.fade} style={{
+            justifyContent: "center",
+            alignItems: "center",
+            textAlign: "center",
+            display: "flex",
+            flexFlow: "column",
+            minHeight: "80vh"
+
+        }}>{text}
+            <div className="step">
+                “ You’ve already taken the most important step,<br></br>
+                coming here “
+            </div>
+            <br></br>
+            <div className="name">
+                - Inner Peace -
+            </div>
         </div>
-      </div>
-  )
+    )
 }
 
 
